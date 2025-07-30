6f5f08340106d00b8c4243828505a3fe425061b0
jerome244 <10485@holbertonstudents.com>
2025-07-30 13:01:42 +0200

    fix: docstrings

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+
+"""
+This module contains a function 'add' that takes two float arguments 
+and returns their sum as a float.
+"""
+
 def add(a: float, b: float) -> float:
     """
     This function takes two float arguments, a and b,
