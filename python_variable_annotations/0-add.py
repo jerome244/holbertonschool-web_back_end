6ff0326140ec8dc2057d58e19216f28dcad0b802
jerome244 <10485@holbertonstudents.com>
2025-07-30 12:58:35 +0200

    feat: push repo, task0 function addition two floats from given arguments

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    This function takes two float arguments, a and b, and returns their sum as a float.
+    """
+    return a + b
