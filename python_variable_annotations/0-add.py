816e438008d34fc40e6f7d424af594ae92bb0cae
jerome244 <10485@holbertonstudents.com>
2025-07-30 13:03:51 +0200

    fix : add blank line 9 for fitting pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -6,6 +6,7 @@ This module contains a function 'add' that takes two
 float arguments and returns their sum as a float.
 """
 
+
 def add(a: float, b: float) -> float:
     """
     This function takes two float arguments, a and b,
