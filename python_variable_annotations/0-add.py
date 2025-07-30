59d9f735fcfe6359b20b8d37a33da1c816853613
jerome244 <10485@holbertonstudents.com>
2025-07-30 12:59:40 +0200

    fix : pycode style line5

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 
 def add(a: float, b: float) -> float:
     """
-    This function takes two float arguments, a and b, and returns their sum as a float.
+    This function takes two float arguments, a and b,
+    and returns their sum as a float.
     """
     return a + b
