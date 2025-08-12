de917c4742469f83b08082d50391f4c21e3cb365
jerome244 <10485@holbertonstudents.com>
2025-08-12 06:58:08 +0200

    fix

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 """ Pymongo """
 
-from typing import List
-from pymongo.collection import Collection
 
-def list_all(mongo_collection: Collection) -> List[dict]:
+def list_all(mongo_collection: object) -> list:
     """List all documents in a collection
 
     Args:
-        mongo_collection (Collection): The pymongo collection object
-
-    Returns:
-        List[dict]: A list of documents (each as a dictionary)
+        mongo_collection (object):
+        pymongo collection object
     """
-    return list(mongo_collection.find({}))
+    return mongo_collection.find({}) if mongo_collection.find({}) else []
