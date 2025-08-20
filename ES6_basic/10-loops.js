7881b8869b063854ab29b70d65eda014d0d2428c
jerome244 <10485@holbertonstudents.com>
2025-08-20 19:32:11 +0200

    diverse tasks

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+// 10-loops.js
+
+export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
+  for (const value of array) {
+    result.push(appendString + value);
+  }
+  return result;
+}
---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,11 @@
+// 7-getBudgetObject.js
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
