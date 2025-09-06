f596c48c4e71a44e8c2e57c8dd6cab93e1a68b4e
jerome244 <10485@holbertonstudents.com>
2025-09-06 10:13:37 +0200

    fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,12 @@
 // 1-stdin.js
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (chunk) => {
-  const name = chunk.toString().trim();
-  if (name.length > 0) {
-    console.log(`Your name is: ${name}`);
-  }
+  // Echo the input as-is (don't trim), so tests comparing \r / \n pass
+  process.stdout.write(`Your name is: ${chunk}`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
